ae15a5f56854f5d5e2348fb60b7387c7c23c8ad7
DarkScheme <paul.informatik@googlemail.com>
2026-01-22 23:53:39 +0100

    added triangle and left right rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -11,14 +12,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    new_clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while 1 > 0:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return    
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = new_clock.tick(60) / 1000
+        
     
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,50 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+
+    def draw(self, screen):
+        list_of_points = self.triangle()
+        pygame.draw.polygon(screen, "white", list_of_points, LINE_WIDTH)
+
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+
+
+
+# /*
+# class CircleShape(pygame.sprite.Sprite):
+#     def __init__(self, x, y, radius):
+#         # we will be using this later
+#         if hasattr(self, "containers"):
+#             super().__init__(self.containers)
+#         else:
+#             super().__init__()
+
+#         self.position = pygame.Vector2(x, y)
+#         self.velocity = pygame.Vector2(0, 0)
+#         self.radius = radius
+# */
